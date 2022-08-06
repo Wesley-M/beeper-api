2e07e3bf8aa5fb53de5388acdbc47c0753965727
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2022-08-05 22:07:04 -0300

    fix: refactor hard coded connection string to Heroku config var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const mongoString = process.env.DATABASE_URL;
+const mongoString = process.env.MONGODB_URI;
 const routes = require("./routes/routes");
 
 mongoose.connect(mongoString);
